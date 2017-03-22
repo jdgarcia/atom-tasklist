be9e8feed3c6a73de8230d327b40e980a0327277
Jose Garcia <joseg@axosoft.com>
2017-03-22 15:43:16 -0700

    initial commit

---
+++ spec/tasklist-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TasklistView from '../lib/tasklist-view';
+
+describe('TasklistView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
